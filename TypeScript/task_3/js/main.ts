36f9f43f11d881ced4856b3fed3f9cc530483832
Steph974-Git <10466@holbertonstudents.com>
2025-09-10 10:29:35 +0200

    feat: add CRUD operations for row management

---
+++ TypeScript/task_3/js/main.ts
@@ -0,0 +1,25 @@
+/// <reference path="./crud.d.ts" />
+import * as CRUD from "./crud";
+
+type RowID = number;
+
+type RowElement = {
+  firstName: string;
+  lastName: string;
+  age?: number;
+};
+
+const row: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva"
+};
+const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+  age: 23
+};
+CRUD.updateRow(newRowID, updatedRow);
+
+CRUD.deleteRow(newRowID);
